d09759182568feda9d1130f9a9df77a58eede5ed
Amadeu Santos <amadeusantos31@gmail.com>
2023-04-06 22:20:17 -0300

    modificando a estrutura do banco de dados

--- sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/perfil/modelo/Perfil.java
+++ sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/perfil/modelo/Perfil.java
@@ -2,28 +2,39 @@ package br.upe.sisepei.sisepei.core.perfil.modelo;
 
 import java.util.List;
 
-import br.upe.sisepei.sisepei.core.usuarioPerfil.modelo.UsuarioPerfil;
+import org.springframework.security.core.GrantedAuthority;
+
+import br.upe.sisepei.sisepei.core.usuario.modelo.Usuario;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
 
 @Data
 @Entity
-public class Perfil {
+public class Perfil implements GrantedAuthority {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
 	
-	@Column(unique = true)
-	private String nome;
+	@Column(nullable = false, unique = true)
+	@Enumerated(EnumType.STRING)
+	private PerfilEnum nome;
 	
-	@OneToMany(mappedBy = "perfil", fetch = FetchType.EAGER)
-	private List<UsuarioPerfil> usuarios; 
+	@ManyToMany(mappedBy="perfis")
+	private List<Usuario> usuarios;
+	
+	@Override
+	public String getAuthority() {
+		return nome.toString();
+	}
 	
 }
--- sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/usuario/modelo/Usuario.java
+++ sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/usuario/modelo/Usuario.java
@@ -1,22 +1,31 @@
 package br.upe.sisepei.sisepei.core.usuario.modelo;
 
+import java.util.Collection;
 import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import br.upe.sisepei.sisepei.core.edital.modelo.Edital;
-import br.upe.sisepei.sisepei.core.usuarioPerfil.modelo.UsuarioPerfil;
+import br.upe.sisepei.sisepei.core.perfil.modelo.Perfil;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
 @Data
-public class Usuario {
+public class Usuario implements UserDetails {
 	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
@@ -28,10 +37,48 @@ public class Usuario {
 	
 	private String senha;
 	
-	@OneToMany(mappedBy = "usuario", fetch = FetchType.EAGER)
-	private List<UsuarioPerfil> perfis;
-	
 	@OneToMany(mappedBy = "coordenador")
 	private List<Edital> editais;
+	
+	@ManyToMany
+	@JoinTable(name= "usuario_perfil",
+			joinColumns = @JoinColumn(name = "usuario_id"),
+			inverseJoinColumns =  @JoinColumn(name = "perfil_id"))
+	private List<Perfil> perfis;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return perfis;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 	
 }
