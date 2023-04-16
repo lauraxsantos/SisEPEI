25a47071b60ce46333a24c2445d18ae00dc37ab9
João Lyra <joaolyra24@gmail.com>
2023-04-16 17:20:39 -0300

    Revert "Alterações e inicio da implementação dos Cookies"
    This reverts commit 71ec223657a351d7ee6a610b3d1e91df6d934f46.

--- sisepei-web/src/index.js
+++ sisepei-web/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { CookiesProvider } from "react-cookie";
 
 //react bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,9 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CookiesProvider>
-        <App />
-      </CookiesProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
