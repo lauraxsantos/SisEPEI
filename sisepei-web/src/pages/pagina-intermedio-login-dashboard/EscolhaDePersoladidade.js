7e78f65b834df2f4883fee73e28e1f9580a08cc1
Camila Claudino <camila.claudino@upe.br>
2023-04-16 20:51:15 -0300

    Criação do dashboard do usuario

--- sisepei-web/src/pages/pagina-intermedio-login-dashboard/EscolhaDePersoladidade.js
+++ sisepei-web/src/pages/pagina-intermedio-login-dashboard/EscolhaDePersoladidade.js
@@ -1,73 +1,72 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './EscolhaDePersonalidade.css';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 
 export function EscolhaDePersonalidade(){
 
     const navigate =  useNavigate();
 
-    const cookie = Cookies.get('token');
+    // const cookie = Cookies.get('token');
 
     const [ce, setCe] = useState(false);
     const [ci, setCi] = useState(false);
     const [cp, setCp] = useState(false);
     const [adm, setAdm] = useState(false);
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        let perfil = cookie.perfis;
+    //     // let perfil = cookie.perfis;
 
-        for (let index = 0; index < perfil.length; index++) {
-            if(perfil[index] === "ADMINISTRADOR"){
-                setAdm(true);
-            }
-            if(perfil[index] === "COORDENADOR_EXTENSAO"){
-                setCe(true);
-            }
-            if(perfil[index] === "COORDENADOR_PESQUISA"){
-                setCp(true);
-            }
-            if(perfil[index] === "COORDENADOR_INOVACAO"){
-                setCi(true);
-            }
-        }
+    //     for (let index = 0; index < perfil.length; index++) {
+    //         if(perfil[index] === "ADMINISTRADOR"){
+    //             setAdm(true);
+    //         }
+    //         if(perfil[index] === "COORDENADOR_EXTENSAO"){
+    //             setCe(true);
+    //         }
+    //         if(perfil[index] === "COORDENADOR_PESQUISA"){
+    //             setCp(true);
+    //         }
+    //         if(perfil[index] === "COORDENADOR_INOVACAO"){
+    //             setCi(true);
+    //         }
+    //     }
         
-    }, []);
+    // }, []);
 
     
    
     return(
-        <>
-        <fieldset id="menu intermedio">
+        <div className="container">
+            <div className="cardBody">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="titulo">Escolha seu perfil</h1>
+                    </div>
+                </div>
+                <div className="buttons">
+                    
+                    <button className="botao" disabled={!ce} onClick={navigate("/paginainicialCoordExtensao") } >Coordenador de Extensão</button>
+                        
+                    <button className="botao" disabled={!cp} onClick={navigate("/paginainicialCoordPesquisa")}>Coordenador de Pesquisa</button>
 
-            <button id="" type="button" onClick={navigate("/paginainicialCoordExtensao")} 
-            disabled={!ce}
-            >Coordenador de Extensão</button>
-
-            <button id="" type="button" onClick={navigate("/paginainicialCoordPesquisa")} 
-            disabled={!cp}
-            >Coordenador de Pesquisa</button>
-
-            <button id="" type="button" onClick={navigate("/paginacoodenadorinovacao")}
-            disabled={!ci}
-            >Coordenador de Inovação</button>
-            
-            {/**
-            //Aqui nesse link pro ADM tem que ver como vai ser pq ele nao tem pagina inicial ne,
-            //ai se precisar, so pra deixar as coisas funcinando, o adm fica somenmte com essa 
-            funcionalidade de mudar poder e pa
-             */}
-            <button id="" type="button" onClick={navigate("/adm/mudanca/permicao")} 
-            disabled={!adm}
-            >Administrador</button>
-
-            <button id="" type="button" onClick={navigate("/paginainicial")}
-            >Usuario Padrão</button>
-
-        </fieldset>
-        </>
-    );
-
+                    <button className="botao" disabled={!ci} onClick={navigate("/paginacoodenadorinovacao")}>Coordenador de Inovação</button>
+                    
+                    {/* Aqui nesse link pro ADM tem que ver como vai ser pq ele nao tem pagina inicial ne,
+                    ai se precisar, so pra deixar as coisas funcinando, o adm fica somenmte com essa 
+                    funcionalidade de mudar poder e pa */}
+             
+                    <button className="botao" disabled={!adm} onClick={navigate("/adm/mudanca/permicao")}>Administrador</button>
+                        
+                    <button className="botao" onClick={navigate("/paginainicial")}>Usuario Padrão</button>
+                    
+                </div>
+            </div>
+        
+        </div> 
+    );        
 
 }
+
+export default EscolhaDePersonalidade;
